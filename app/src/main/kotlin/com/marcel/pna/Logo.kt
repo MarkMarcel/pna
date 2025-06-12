70ffb4929a1f32c86e2955e0b3953b6d252d2768
MarkMarcel <freelancemarcel18@gmail.com>
2025-06-12 20:25:19 +0000

    app - added ability to change logo color filter

--- app/src/main/kotlin/com/marcel/pna/Logo.kt
+++ app/src/main/kotlin/com/marcel/pna/Logo.kt
@@ -1,38 +1,44 @@
 package com.marcel.pna
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.height
-import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.marcel.pna.theme.PNAMTheme
 
 @Composable
 fun PNAMLogo(
     modifier: Modifier = Modifier,
     requiredHeight: Dp = 48.dp,
-    tint: Color = LocalContentColor.current,
+    tint: Color = MaterialTheme.colorScheme.onSurface,
 ) {
     // Matches the image resource dimensions
     val aspectRatio = 950f / 427f
-    Box(
+    Image(
+        painter = painterResource(id = R.mipmap.logo),
+        colorFilter = ColorFilter.tint(tint),
+        contentDescription = stringResource(R.string.app_logo_content_description),
+        contentScale = ContentScale.Fit,
         modifier = modifier
             .height(requiredHeight)
-            .aspectRatio(aspectRatio)
-    ) {
-        Image(
-            painter = painterResource(id = R.mipmap.logo),
-            contentDescription = stringResource(R.string.app_logo_content_description),
-            modifier = Modifier
-                .matchParentSize(),
-            contentScale = ContentScale.Fit,
-        )
+            .aspectRatio(aspectRatio),
+    )
+}
+
+@Preview
+@Composable
+fun PNAMLogoPreview() {
+    PNAMTheme {
+        PNAMLogo()
     }
 }
